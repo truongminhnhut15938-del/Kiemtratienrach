5be631ec25b451fdf83cde4f16bc9e723379836c
truongminhnhut15938-del <truongminhnhut15938@gmail.com>
2026-07-21 13:02:03 +0700

    Update perspective.py
    Modul 2 part 2

--- services/perspective.py
+++ services/perspective.py
@@ -47,3 +47,51 @@ def find_largest_contour(edge):
     largest = max(contours, key=cv2.contourArea)
 
     return largest
+def order_points(pts):
+    """
+    Sắp xếp 4 điểm theo thứ tự:
+    Top-Left, Top-Right,
+    Bottom-Right, Bottom-Left
+    """
+
+    rect = np.zeros((4, 2), dtype="float32")
+
+    s = pts.sum(axis=1)
+
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+
+    diff = np.diff(pts, axis=1)
+
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+
+    return rect
+
+
+def get_four_points(contour):
+    """
+    Lấy 4 góc của contour.
+    """
+
+    peri = cv2.arcLength(contour, True)
+
+    approx = cv2.approxPolyDP(
+        contour,
+        0.02 * peri,
+        True
+    )
+
+    # Nếu đã tìm được đúng 4 điểm
+    if len(approx) == 4:
+        pts = approx.reshape(4, 2)
+        return order_points(pts)
+
+    # Nếu không đủ 4 điểm thì dùng hình chữ nhật bao ngoài
+    rect = cv2.minAreaRect(contour)
+
+    box = cv2.boxPoints(rect)
+
+    box = np.array(box, dtype="float32")
+
+    return order_points(box)
